dcdbe5f5bf8de08b72adbd7dd04f2bfdf57abdd7
qinshiheng <qinshiheng@yeah.net>
2014-10-17 18:37:35 +0800

    lalal

--- src/com/parnas/kwic/data/settler/AlphaSort.java
+++ src/com/parnas/kwic/data/settler/AlphaSort.java
@@ -1,8 +1,23 @@
 package com.parnas.kwic.data.settler;
 
-public class AlphaSort {
+import java.util.ArrayList;
+import java.util.Collections;
 
-	public AlphaSort(){
-		
+public class AlphaSort {
+	private ArrayList<String> dataString;
+	public AlphaSort(ArrayList<String> argString){
+		this.dataString = argString;
+	}
+	
+	public String Sort(){
+		//调用Java自带的方法进行排序
+		Collections.sort(dataString);
+		String sortedString = "";
+		for (int i = 0; i < dataString.size(); i++) {
+			sortedString += dataString.get(i) + "\n";
+		}
+		return sortedString;
 	}
 }
+
+
